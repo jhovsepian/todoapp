8161c26aba1d1c5d4e54552235f8dff93709c41c
Jacqueline Hovsepian <jhovsepian1018@gmail.com>
2015-04-14 08:51:44 -0700

    milestone 2 added index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Jacky's To-Do App</title>
+<link rel="stylesheet" type="text/css" href="css/main.css">
+</head>
+<body>
+	<div class="wrap">
+
+	</div>
+</body>
+</html>
